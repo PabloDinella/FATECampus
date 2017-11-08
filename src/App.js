6fa44302d038a7810e047c6ba393f5ae5962d5d2
Pablo R. Dinella <pablo.dinella@gmail.com>
2017-11-08 16:05:16 -0200

    Add markers e GroundOverlay

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ class App extends Component {
       <div className="App">
         <TopBar />
         <header className="App-header">
-          <MapView />
+          {/* <MapView /> */}
+          <MapView
+            isMarkerShown
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+            //AIzaSyDlDDOFcLe3m6SlveVfAS7dARz1ZDeNN8o
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `400px` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react'
+import overlayImage from '../assets/map-overlay.png'
+import {withScriptjs, withGoogleMap, GoogleMap, Marker, GroundOverlay} from "react-google-maps"
 
 import withStyles from '../support/withStyles'
 
-export default class MapView extends Component {
-  // constructor() {
-  //
-  // }
+const MapView = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={18}
+    defaultCenter={{lat: -23.529528, lng: -46.632384}}
+  >
+    {props.isMarkerShown &&
+      <div>
+        <Marker position={{lat: -23.529591, lng: -46.632572}} />
+        <Marker position={{lat: -23.530062, lng: -46.632454}} />
+        <Marker position={{lat: -23.529217, lng: -46.632480}} />
+        <Marker position={{lat: -23.529984, lng: -46.632901}} />
+        <Marker position={{lat: -23.529700, lng: -46.633123}} />
+        <Marker position={{lat: -23.528934, lng: -46.632990}} />
+        <Marker position={{lat: -23.529008, lng: -46.632791}} />
+      </div>
+    }
+    <GroundOverlay
+      defaultUrl={overlayImage}
+      defaultBounds={new window.google.maps.LatLngBounds(
+        new window.google.maps.LatLng(-23.530292, -46.633573),
+        new window.google.maps.LatLng(-23.528390, -46.631799)
+      )}
+    />
+  </GoogleMap>
+))
 
-  render() {
-    return <div style={{backgroundColor: 'red'}}>
-      asdf
-    </div>
-  }
-}
-
-// export default withStyles('MapView', styles, MapView)
+export default MapView
